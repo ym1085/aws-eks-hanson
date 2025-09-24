2196f5b3e90b7c9dbd3506353e3b5f5f8a0fbeec
YOUNGMINKIM <Youngmin1085@gmail.com>
2025-09-24 23:54:21 +0900

    fix: sh 수정
    1. shell script 수정

--- eks-hands-on/00-utils/alb-controller/add-aws-helm-repo.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# helm install aws-lb-controller eks/aws-load-balancer-controller
-# helm install aws-lb-controller https://aws.github.io/eks-charts/aws-load-balancer-controller
-helm repo add eks https://aws.github.io/eks-charts
-helm repo update
